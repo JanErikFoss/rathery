c08e2c71e9c9f4737046c446eccf8d5e15516277
Jan Erik Sola Foss <Jani@Jans-iMac.local>
2017-01-27 17:06:40 +0100

    Improved Lobby.js by removing a lot of unused code

--- app/components/Lobby.js
+++ app/components/Lobby.js
@@ -5,48 +5,16 @@ import Game from "./Game/Game"
 import Chat from "./Chat/Chat"
 
 export default class Lobby extends Component {
-  constructor(props){
-    super();
-    this.state = {
-      lobbyState: 0
-    };
-
-  }
-
   render() {
     return (
       <View style={styles.container}>
 
-        <Game {...this.props} onScoreChanged={this.onScoreChanged.bind(this)}/>
-        <Chat {...this.props} uid={this.uid} />
+        <Game {...this.props} />
+        <Chat {...this.props} />
 
       </View>
     );
   }
-
-  shopPressed(){
-    console.log("Cash pressed");
-    this.setState({lobbyState: 1});
-  }
-  shopFinished(){
-    console.log("Shop finished");
-    this.setState({lobbyState: 0}); 
-  }
-
-  ladderPressed(){
-    console.log("Ladder pressed");
-    this.setState({lobbyState: 2});
-  }
-  ladderFinished(){
-    console.log("Ladder finished");
-    this.setState({lobbyState: 0});
-  }
-
-  onScoreChanged(score){
-    console.log("Score changed: " + score);
-    this.setState({score: score || 0})
-  }
-
 }
 
 const styles = StyleSheet.create({
